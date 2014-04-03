473095f344d41e84484c65db33668f88cf942650
Giovanni Bassi <giggio@email>
2014-04-03 10:11:18 -0300

    onload

--- index.js
+++ index.js
@@ -8,17 +8,21 @@ var usingBuffer = function() {
   fs.readFile(__dirname + '/untitled.png', function(err, bytes){
     if (err) throw err;
     img = new Image;
+    img.onload = function() {
+      ctx.drawImage(img, 0, 0, img.width / 4, img.height / 4);
+      console.log(canvas.toDataURL('image/png'));
+    }
     img.src = bytes;
-    ctx.drawImage(img, 0, 0, img.width / 4, img.height / 4);
-    console.log(canvas.toDataURL('image/png'));
   });
 }
 
 var usingFileName = function() {
   img = new Image;
-  img.src = __dirname + '/untitled.png'
+  img.onload = function() {
     ctx.drawImage(img, 0, 0, img.width / 4, img.height / 4);
-  console.log(canvas.toDataURL('image/png'));
+    console.log(canvas.toDataURL('image/png'));
+  }
+  img.src = __dirname + '/untitled.png'
 }
 
 usingFileName();
